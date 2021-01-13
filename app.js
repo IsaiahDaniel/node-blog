d21e150d25ec84670d0ccc9d99d90a116dfc8f77
isaiah Daniel <isaiahdaniel203@gmail.com>
2021-01-13 03:26:44 +0100

    add controllers and express router

--- app.js
+++ app.js
@@ -1,14 +1,11 @@
 const express = require('express');
-const path = require('path');
 const morgan = require('morgan');
 const mongoose =  require('mongoose');
 
 const app = express();
-
 app.set('view engine', 'ejs');
 
 const db = require('./config/DB').mongoURI;
-const Blog = require('./models/blogs');
 
 mongoose
   .connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
@@ -16,8 +13,8 @@ mongoose
   .catch(err => console.log(err));
 
 app.use(express.static('public'));
+app.use(express.urlencoded({ extended: false }))
 app.use(morgan('dev'));
-
 
 app.get('/', (req, res) => {
   res.redirect('/blogs');
@@ -27,19 +24,13 @@ app.get('/about', (req, res) => {
   res.render('about', { title: 'about' });
 });
 
-app.get('/blogs/create', (req, res) => {
-  res.render('create', { title: 'blogs' });
-});
+// Blog routes
 
-app.get('/blogs', (req, res) => {
-  Blog.find()
-    .sort({ createdAt: -1 })
-    .then(result => res.render('index', { title: 'All Blogs', blogs: result }))
-    .catch(err => console.log(err));
-});
+app.use('/', require('./routes/blogRoutes'));
+
 
 app.use((req, res) => {
   res.status(404).render('404', { title: 'Not found' });
-});
+}); 
 
 app.listen(5000, () => console.log('server running on port 5000')); 
